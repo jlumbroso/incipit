6bdd235b109dec11fb2a268c9bec7bb4f2068fea
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2021-03-13 16:26:40 -0500

    Fixed terminology staves -> systems of staves, v2021.3.2

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# `incipit` — extract staves from musical scores
+# `incipit` — extract systems of staves from musical scores
 
 This command-line tool (and Python package) allows for the easy extraction of
-staves from musical scores. In particular, it makes it easy to extract the first
-staff of any musical score that is in B&W format, and has a reasonable amount
-of background noise.
+systems of staves from musical scores. In particular, it makes it easy to extract
+the first system of any musical score that is in B&W format, and has a noiseless
+enough background
 
 This tool was built to automatically generate incipits for large sets of scores,
 like those downloaded from IMSLP. It was specifically built to create an index
-of sonatas by Domenico Scarlatti.
+of the 555 keyboard sonatas by Domenico Scarlatti, in a related project.
 
 ## Installation
 
@@ -23,27 +23,29 @@ runnable, called `incipit`.
 ```
 Usage: incipit [OPTIONS] INPUT
 
-  Extract the first (or any) staff from a black-and-white modern musical
-  score, either available as an image file or PDF. For instance,
+  Extract the first (or any) system of staves from a black-and-white modern
+  musical score, either available as an image file or PDF. For instance,
 
       $ incipit -o "555-incipit.png" ./input/pdf/555-gilbert.pdf
 
-  will extract the first staff of sheet music `555-gilbert.pdf' as the image
-  `555-incipit.png`. And,
+  will extract the first system of staves of the sheet music
+  `555-gilbert.pdf' as the image `555-incipit.png`. And,
 
       $ incipit --output "1__{}.png" -# '0,-1' ./input/pdf/1-gilbert.pdf
 
-  will extract the first and last staves of `1-gilbert.pdf` and output these
-  as the images `1__0.png' (first staff) and `1__10.png' (last staff).
+  will extract the first and last systems of staves of `1-gilbert.pdf` and
+  output these as the images `1__0.png' (first system) and `1__10.png' (last
+  system).
 
 Options:
-  -a, --audit                   Visualize staff detection across document
-  -c, --count                   Output number of detected staves
+  -a, --audit                   Visualize system detection across document
+  -c, --count                   Output number of detected systems
   -p, --pages TEXT              List of pages to process (e.g., '0', '0,-1')
-  -#, --staves TEXT             List of staves to extract (e.g., '0', '0,-1')
-  -h, --height-threshold FLOAT  % of height threshold for staff detection
-  -w, --width-threshold FLOAT   % of width threshold for staff detection
+  -#, --systems TEXT            List of systems to extract (e.g., '0', '0,-1')
+  -h, --height-threshold FLOAT  % of height threshold for system detection
+  -w, --width-threshold FLOAT   % of width threshold for system detection
   -o, --output TEXT             Output file pattern
   -v, --verbose                 Print debug information
+  --version                     Show the version and exit.
   --help                        Show this message and exit.
 ```
--- src/incipit/cli.py
+++ src/incipit/cli.py
@@ -1,6 +1,5 @@
 
 import os
-import typing
 
 import click
 import click_help_colors
@@ -12,33 +11,35 @@ import incipit.heuristics
 
 
 @click.command(
+    # Green is the best color in the world.
     cls=click_help_colors.HelpColorsCommand,
     help_headers_color='bright_green',
     help_options_color='green'
 )
-@click.option("-a", "--audit", is_flag=True, help="Visualize staff detection across document")
-@click.option("-c", "--count", is_flag=True, help="Output number of detected staves")
+@click.option("-a", "--audit", is_flag=True, help="Visualize system detection across document")
+@click.option("-c", "--count", is_flag=True, help="Output number of detected systems")
 @click.option("-p", "--pages", type=str, help="List of pages to process (e.g., '0', '0,-1')")
-@click.option("-#", "--staves", type=str, help="List of staves to extract (e.g., '0', '0,-1')")
-@click.option("-h", "--height-threshold", type=float, default=10.0, help="% of height threshold for staff detection")
-@click.option("-w", "--width-threshold", type=float, default=70.0, help="% of width threshold for staff detection")
+@click.option("-#", "--systems", type=str, help="List of systems to extract (e.g., '0', '0,-1')")
+@click.option("-h", "--height-threshold", type=float, default=10.0, help="% of height threshold for system detection")
+@click.option("-w", "--width-threshold", type=float, default=70.0, help="% of width threshold for system detection")
 @click.option("-o", "--output", help="Output file pattern")
 @click.option("-v", "--verbose", is_flag=True, default=False, help="Print debug information")
+@click.version_option(incipit.__version__)
 @click.argument("input", type=click.Path(exists=True))
-def main(audit, count, pages, staves, height_threshold, width_threshold, output, verbose, input):
+def main(audit, count, pages, systems, height_threshold, width_threshold, output, verbose, input):
     """
-    Extract the first (or any) staff from a black-and-white modern
+    Extract the first (or any) system of staves from a black-and-white modern
     musical score, either available as an image file or PDF. For instance,
 
         $ incipit -o "555-incipit.png" ./input/pdf/555-gilbert.pdf
 
-    will extract the first staff of sheet music `555-gilbert.pdf' as the
-    image `555-incipit.png`. And,
+    will extract the first system of staves of the sheet music `555-gilbert.pdf'
+    as the image `555-incipit.png`. And,
 
         $ incipit --output "1__{}.png" -# '0,-1' ./input/pdf/1-gilbert.pdf
 
-    will extract the first and last staves of `1-gilbert.pdf` and output
-    these as the images `1__0.png' (first staff) and `1__10.png' (last staff).
+    will extract the first and last systems of staves of `1-gilbert.pdf` and output
+    these as the images `1__0.png' (first system) and `1__10.png' (last system).
     """
 
     if not verbose:
@@ -47,10 +48,10 @@ def main(audit, count, pages, staves, height_threshold, width_threshold, output,
     loguru.logger.debug(
         (
             "CLI called with: output={output}, audit={audit}, pages={pages}, "
-            "staves={staves}, count={count}, input={input},"
+            "systems={systems}, count={count}, input={input},"
             "width_treshold={width_threshold}, height_threshold={height_threshold}"
         ),
-        output=output, audit=audit, pages=pages, staves=staves, count=count, input=input,
+        output=output, audit=audit, pages=pages, systems=systems, count=count, input=input,
         width_threshold=width_threshold, height_threshold=height_threshold,
     )
 
@@ -81,48 +82,48 @@ def main(audit, count, pages, staves, height_threshold, width_threshold, output,
 
         return
 
-    if staves is not None:
-        staves = list(map(int, staves.split(",")))
+    if systems is not None:
+        systems = list(map(int, systems.split(",")))
         loguru.logger.debug(
-            "requested staves={staves}",
-            staves=staves,
+            "requested systems={systems}",
+            systems=systems,
         )
 
     loguru.logger.debug(
-        "requested pages={pages}; staves={staves}",
-        pages=pages, staves=staves,
+        "requested pages={pages}; systems={systems}",
+        pages=pages, systems=systems,
     )
 
-    if count and output is None and staves is None:
+    if count and output is None and systems is None:
         loguru.logger.debug("COUNT method activated")
-        staves = incipit.heuristics.detect_staves_from_input_document(
+        systems = incipit.heuristics.detect_staves_from_input_document(
             input_path=input,
             width_proportion=width_threshold,
             height_proportion=height_threshold,
             page_numbers_to_keep=pages,
         )
-        print(len(staves))
+        print(len(systems))
         return
 
     if output is None:
         basename, _ = os.path.splitext(os.path.basename(input))
-        output = "{}_staff_{{:02}}.png".format(basename)
+        output = "{}_system_{{:02}}.png".format(basename)
         loguru.logger.debug("using '{}' as output pattern", output)
 
-    extracted_staves = incipit.heuristics.extract_staves_from_input_document(
+    extracted_systems = incipit.heuristics.extract_staves_from_input_document(
         input_path=input,
         output_filename_pattern=output,
-        staves_to_keep=staves,
+        staves_to_keep=systems,
         width_proportion=width_threshold,
         height_proportion=height_threshold,
         page_numbers_to_keep=pages,
     )
 
-    extract_staves_filenames = list(map(lambda x: x[0], extracted_staves))
-    loguru.logger.debug("Extracted staves: {}", ",".join(extract_staves_filenames))
+    extracted_systems_filenames = list(map(lambda x: x[0], extracted_systems))
+    loguru.logger.debug("Extracted staves: {}", ",".join(extracted_systems_filenames))
 
     if count:
-        print(len(extract_staves_filenames))
+        print(len(extracted_systems_filenames))
 
 
 if __name__ == '__main__':
