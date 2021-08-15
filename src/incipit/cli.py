2043bf95580288eca8bf7ec6d5d664cceb29db43
zhanganlun@gmail.com <zhanganlun@gmail.com>
2021-08-15 11:51:36 -0400

    remove unnecessary debug statements for pull request

--- src/incipit/cli.py
+++ src/incipit/cli.py
@@ -5,12 +5,9 @@ import click
 import click_help_colors
 import loguru
 
-import src.incipit
-import src.incipit.processing
-import src.incipit.heuristics
-
-systemNumberInPDFList1 = [11,11,	14,	13,	12,	12,	21,	12,	12,	12,	10,	24,	23,	13,	19,	23,	22,	20,	21,	16,	24,	18,	23,	24,	19,	22,	18,	22,	26,	24,	23,	4,	21,	5,	20,	18,	18,	15,	17,	4,	27,	5,	16,	22,	18,	28,	24,	22,	31,	24,	17,	18,	22,	19,	22,	21,	24,	19,	10,	11,	32,	19,	11,	10,	12,	11,	10,	19,	12,	11,	10,	12,	17,	9,	11,	12,	20,	11,	16,	6,	33,	31,	23,	18,	17,	21,	12,	47,	32,	49,	37,	14,	29,	5,	8,	24,	30,	19,	21,	16,	20,	18,	17,	33,	28,	15,	24,	20,	20,	20,	18,	20,	28,	30,	33,	20,	34,	23,	32,	20,	33,	22,	22,	26,	18,	22,	24,	26,	22,	18,	20,	21,	22,	26,	23,	23,	22,	21,	26,	14,	28,	19,	20,	11,	20,	17,	22,	12,	12,	18,	20,	17,	10,	19,	20,	14,	22,	15,	16,	22,	16,	25,	10,	20,	15,	17,	24,	21,	23,	25,	11,	30,	20,	31,	20,	28,	14,	12,	15,	18,	22,	19,	20,	24,	12,	15,	25,	27,	21,	18,	24,	24,	21,	23,	23,	17,	12,	18,	21,	20,	21,	25,	26,	11,	48,	26,	20,	10,	22,	12,	34,	25,	24,	23,	23,	32,	26,	19,	20,	30,	27,	19,	22,	22,	23,	18,	27,	20,	20,	24,	22,	22,	24,	20,	21,	20,	20,	15,	20,	42,	19,	22,	17,	20,	24,	20,	20,	28,	28,	21,	17,	18,	18,	21,	21,	21,	19,	24,	20,	37,	29,	22,	23,	36,	32,	19,	18,	26,	18,	28,	19,	22,	20,	15,	17,	20,	12,	18,	21,	21,	17,	27,	20,	24,	21,	16,	15,	17,	12,	18,	17,	18,	30,	21,	22,	33,	22,	28,	15,	19,	21,	30,	21,	12,	20,	23,	18,	18,	20,	22,	20,	20,	18,	32,	16,	23,	22,	20,	23,	21,	15,	17,	12,	20,	17,	19,	16,	21,	29,	12,	20,	22,	15,	17,	22,	18,	22,	17,	20,	20,	12,	18,	17,	16,	26,	15,	16,	18,	22,	17,	36,	23,	20,	20,	21,	35,	28,	20,	20,	26,	15,	17,	18,	22,	18,	20,	18,	34,	25,	20,	16,	15,	19,	21,	15,	15,	17,	21,	15,	18,	20,	18,	15,	13,	15,	22,	14,	25,	16,	24,	11,	30,	12,	31,	20,	30,	15,	25,	16,	15,	22,	33,	22,	35,	19,	20,	12,	16,	23,	20,	11,	20,	12,	29,	10,	22,	50,	23,	16,	27,	21,	30,	21,	20,	20,	22,	13,	12,	15,	15,	4,	16,	27,	20,	18,	16,	15,	24,	22,	12,	20,	18,	22,	20,	19,	12,	20,	20,	20,	20,	11,	18,	12,	26,	25,	19,	16,	23,	21,	36,	25,	18,	17,	20,	15,	21,	22,	25,	22,	31,	15,	15,	24,	18,	24,	24,	24,	24,	32,	24,	16,	22,	16,	20,	18,	30,	20,	20,	18,	23,	21,	24,	24,	25,	20,	24,	21,	20,	24,	23,	28,	20,	18,	15,	15,	16,	22,	20,	20,	20,	21,	20,	20,	15,	20,	21,	24,	24,	22,	20,	20,	21,	11,	20,	18,	23,	17,	15,	17,	21,	23,	22,	22,	11,	21,	21,	22,	20,	24,	22,	22,	22,	20,	10,	22,	20,	28,	20,	22,	21,	23,	20,	21,	21,	20,	20,	20,	11]
-systemNumberInPDFList2 = [3,	5,	5,	6,	6,	6,	5,	5,	6,	6,	7,	7,	5,	6,	6,	7,	6,	6,	6,	6,	6,	7,	5,	6,	6,	6,	6,	6,	6,	7,	6,	6,	5,	6,	6,	6,	6,	7,	5,	5,	5,	6,	6,	6,	6,	6,	6,	6,	5,	6,	6,	6,	6,	5,	5,	6,	5,	5,	6,	5,	5,	4,	6,	6,	5,	5,	5,	5,	6,	6,	6,	6,	6,	6,	6,	6,	5,	6,	6,	6,	6,	6,	6,	6,	5,	5,	5,	6,	6,	6,	6,	6,	6,	6,	6,	5,	6,	5,	5,	6,	5,	5,	6,	5,	6,	5,	6,	5,	5,	5,	5,	5,	5,	5,	5,	5,	6,	5,	5,	4,	5,	5,	7,	6,	6,	6,	6,	5,	6,	6,	6,	6,	5,	6,	5,	5,	4,	5,	5,	6,	6,	6,	6,	5,	5,	6,	6,	7,	6,	5,	5,	5,	5,	5,	5,	5,	7,	2,	2,	2,	2,	2,	5,	5,	5,	6,	6,	6,	6,	6,	6,	5,	5,	6,	6,	5,	5,	5,	6,	6,	6,	5,	5,	6,	6,	5,	5,	6,	6,	6,	6,	7,	6,	6,	6,	5,	5,	4,	5,	5,	5,	5,	5,	6,	6,	4,	7,	5,	7,	5,	6,	6,	6,	6,	6,	5,	5,	5,	5,	6,	5,	5,	5,	4,	5,	6,	6,	5,	5,	5,	5,	5,	5,	5,	6,	5,	5,	6]
+import incipit
+import incipit.processing
+import incipit.heuristics
 
 @click.command(
     # Green is the best color in the world.
@@ -26,7 +23,7 @@ systemNumberInPDFList2 = [3,	5,	5,	6,	6,	6,	5,	5,	6,	6,	7,	7,	5,	6,	6,	7,	6,	6,	
 @click.option("-w", "--width-threshold", type=float, default=55.0, help="% of width threshold for system detection")
 @click.option("-o", "--output", help="Output file pattern")
 @click.option("-v", "--verbose", is_flag=True, default=False, help="Print debug information")
-@click.version_option(src.incipit.__version__)
+@click.version_option(incipit.__version__)
 @click.argument("input", type=click.Path(exists=True))
 def main(audit, count, pages, systems, height_threshold, width_threshold, output, verbose, input):
     """
@@ -72,7 +69,7 @@ def main(audit, count, pages, systems, height_threshold, width_threshold, output
             output = "{}_audit_p{{:02}}.png".format(basename)
             loguru.logger.debug("using '{}' as output pattern", output)
 
-        staff_audit_filenames = src.incipit.heuristics.write_staff_detection_images_from_input_document(
+        staff_audit_filenames = incipit.heuristics.write_staff_detection_images_from_input_document(
             input_path=input,
             output_filename_pattern=output,
             width_proportion=width_threshold,
@@ -95,49 +92,23 @@ def main(audit, count, pages, systems, height_threshold, width_threshold, output
         "requested pages={pages}; systems={systems}",
         pages=pages, systems=systems,
     )
-
-    totalError = 0
-    offset2 = 14
-    offset1 = 1
     if count and output is None and systems is None:
         loguru.logger.debug("COUNT method activated")
-        for i in range(offset2, 252):
-          input =   "C:\\Users\\user\\Google Drive\\projects\\python\\pdf2\\page" + str(i).zfill(3) + ".pdf"
-          systems = src.incipit.heuristics.detect_staves_from_input_document(
+        systems = incipit.heuristics.detect_staves_from_input_document(
             input_path=input,
             width_proportion=width_threshold,
             height_proportion=height_threshold,
             page_numbers_to_keep=pages,
-          )
-          if( len(systems) - systemNumberInPDFList2[i-offset2] != 0 ):
-            print(str(len(systems)) + "\t" + str(len(systems) - systemNumberInPDFList2[i-offset2]) + "\t" + input )
-          else:
-            print("matched " + str(i) + ".pdf")
-          totalError += abs(len(systems) - systemNumberInPDFList2[i-offset2])
-        for i in range(offset1, 559):
-          input = "C:\\Users\\user\\Google Drive\\projects\\python\\pdf\\" + str(i) + ".pdf"
-          systems = src.incipit.heuristics.detect_staves_from_input_document(
-            input_path=input,
-            width_proportion=width_threshold,
-            height_proportion=height_threshold,
-            page_numbers_to_keep=pages,
-          )
-          if( len(systems) - systemNumberInPDFList1[i-offset1] != 0 ):
-            print(str(len(systems)) + "\t" + str(len(systems) - systemNumberInPDFList1[i-offset1]) + "\t" + input )
-          else:
-            print("matched " + str(i) + ".pdf")
-          totalError += abs(len(systems) - systemNumberInPDFList1[i-offset1])
-
-        print(" total error " + str(totalError))
+        )
+        print(len(systems))
         return
-
 
     if output is None:
         basename, _ = os.path.splitext(os.path.basename(input))
         output = "{}_system_{{:02}}.png".format(basename)
         loguru.logger.debug("using '{}' as output pattern", output)
 
-    extracted_systems = src.incipit.heuristics.extract_staves_from_input_document(
+    extracted_systems = incipit.heuristics.extract_staves_from_input_document(
         input_path=input,
         output_filename_pattern=output,
         staves_to_keep=systems,
